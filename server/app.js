867b9142722dd214ec7542c10e989d41287f6292
Seth Lassen <sethmlassen@gmail.com>
2020-12-28 16:13:25 -0800

    React setup complete

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 // app.get('/', (req, res) => {
 //   res.send('hello')
